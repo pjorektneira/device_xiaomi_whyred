86be07aa4229a835695206bd412a3e3a9ad39770
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2020-01-21 14:37:22 +0530

    whyred: Add powerhint.json for sdm636
    Change-Id: I3591038a29326b15ce00de0051208219d9f3830d
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- device.mk
+++ device.mk
@@ -364,7 +364,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi_sdm660-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    $(DEVICE_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
+    $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
