85fd4c89d858b72e5bbf522d4fd19f3a7c95bdef
Reinazhard <muh.alfarozy@gmail.com>
2021-04-06 08:42:54 +0000

    whyred: revengeos bringup
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_whyred.mk
+    $(LOCAL_DIR)/revengeos_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_whyred-user \
-    lineage_whyred-userdebug \
-    lineage_whyred-eng
+    revengeos_whyred-user \
+    revengeos_whyred-userdebug \
+    revengeos_whyred-eng
