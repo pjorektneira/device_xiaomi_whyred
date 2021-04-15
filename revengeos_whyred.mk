594ecf703f59633c8a7628022f89a86d3488d214
MocaRafee <harif.rp05@gmail.com>
2021-04-15 20:24:40 +0700

    whyred: set build fingerprint to april redfin
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>

--- revengeos_whyred.mk
+++ revengeos_whyred.mk
@@ -39,6 +39,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="whyred-user 9 PKQ1.180904.001 V12.0.2.0.PEIMIXM release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys"
 
-BUILD_FINGERPRINT := xiaomi/whyred/whyred:9/PKQ1.180904.001/V12.0.2.0.PEIMIXM:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
