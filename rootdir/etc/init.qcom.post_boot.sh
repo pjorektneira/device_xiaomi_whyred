4bba22d4b39b0ba70b65ffccc88b1df8fea9a5f3
Martin Liu <liumartin@google.com>
2021-07-01 11:55:02 +0800

    whyred: init: disable watermark_boost feature
    In newer kernel, it introduces watermark boost feature
    to enhance memory fragmentation problems. This feature
    will dynamically boost/down watermark level which conflicts
    with LMKD assumption and causing LMKD not to work properly.
    Disabling this feature first until we address it.
    
    Bug: 189938926
    Test: check vm parameter.
    Signed-off-by: Martin Liu <liumartin@google.com>
    Change-Id: I6db5163f843f1129080bb944df7cbb8c799a58e0
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -349,7 +349,7 @@ else
 
     # Disable wsf for all targets beacause we are using efk.
     # wsf Range : 1..1000 So set to bare minimum value 1.
-    echo 1 > /proc/sys/vm/watermark_scale_factor
+    echo 0 > /proc/sys/vm/watermark_scale_factor
 
     configure_zram_parameters
 
