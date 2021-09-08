70f03102de6c095978f00d449442552966b12c3f
Reinazhard <reinazhard@gmail.com>
2021-09-08 02:25:31 +0000

    whyred: PE bringup
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_whyred.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_whyred-user \
-    lineage_whyred-userdebug \
-    lineage_whyred-eng
+    $(LOCAL_DIR)/aosp_whyred.mk
