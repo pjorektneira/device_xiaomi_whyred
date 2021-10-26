bcd453c53bde0ed83f82332f699954f21bd74087
Reinazhard <reinazhard@gmail.com>
2021-10-26 02:29:27 +0000

    whyred: SELinux Enforcing
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,8 +110,7 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.configfs=true \
     androidboot.usbcontroller=a800000.dwc3 \
     loop.max_part=7 \
-    kpti=off \
-    androidboot.selinux=permissive
+    kpti=off
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
